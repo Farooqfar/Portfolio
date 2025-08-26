938913f566e7aa4be497947152f8908b88deeb4b
Farooqfar <info4895@gmail.com>
2025-08-26 19:56:26 +0500

    new

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,11 +1,11 @@
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    return (
         <nav className="w-full h-auto p-8 flex items-center justify-center">
             <ul className="flex justify-center items-center gap-10">
-                <li className="text-white text-2xl text-black cursor-pointer">Home</li>
-                <li className="text-white text-2xl text-black cursor-pointer">Work</li>
-                <li className="text-white text-2xl text-black cursor-pointer">About</li>
-                <li className="text-white text-2xl text-black cursor-pointer">Contact</li>
+                <li className="text-white text-2xl  font-black cursor-pointer">Home</li>
+                <li className="text-white text-2xl  font-black cursor-pointer">Work</li>
+                <li className="text-white text-2xl  font-black cursor-pointer">About</li>
+                <li className="text-white text-2xl fontt-black cursor-pointer">Contact</li>
             </ul>
         </nav>
     )
