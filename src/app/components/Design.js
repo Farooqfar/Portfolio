e7997baa6907ea62f22a57926357f40489539c8a
Farooqfar <info4895@gmail.com>
2025-08-31 19:51:27 +0500

    commit

--- src/app/components/Design.js
+++ src/app/components/Design.js
@@ -8,60 +8,60 @@ export default function Design() {
     return (
         <main>
             <section className="w-full h-auto mt-32">
-                <h1 className="text-left font-[cursive] text-[#DADADA] text-[250px] leading-[170px]">Approach</h1>
-                <h1 className="text-right leading-[170px] uppercase text-transparent text-[170px] font-black [-webkit-text-stroke:2px_#DADADA] scale-y-150 tracking-tighter">Ideas  into <br /> Impact</h1>
-                <p className="text-left uppercase text-[#DADADA] font-black w-[400px] text-2xl">It’s not just about making things pretty — it’s about making them count.</p>
+                <h1 className="text-left font-[cursive] text-[#DADADA] text-[250px] leading-[170px] max-lg:text-[160px] max-lg:mb-20 max-sm:text-[70px]">Approach</h1>
+                <h1 className="text-right leading-[170px] uppercase text-transparent text-[170px] font-black [-webkit-text-stroke:2px_#DADADA] scale-y-150 tracking-tighter max-sm:text-[60px] max-sm:leading-[60px]">Ideas  into <br /> Impact</h1>
+                <p className="text-left uppercase text-[#DADADA] font-black w-[400px] text-2xl max-lg:mt-32 max-sm:text-sm">It’s not just about making things pretty — it’s about making them count.</p>
             </section>
             <section className="w-auto h-auto mt-32 flex gap-32 overflow-hidden">
                 <div className="w-100 flex flex-col items-center">
-                    <GiArchiveResearch className="text-white text-[250px] text-center" />
-                    <h1 className="text-white text-center text-3xl font-black">-1-</h1>
-                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150">research</h1>
-                    <p className="text-white text-2xl text-center mt-10">Digging into insights and user needs to uncover the story behind the solution.</p>
+                    <GiArchiveResearch className="text-white text-[250px] text-center max-sm:text-[130px]" />
+                    <h1 className="text-white text-center text-3xl font-black max-sm:text-2xl">-1-</h1>
+                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150 max-sm:text-4xl">research</h1>
+                    <p className="text-white text-2xl text-center mt-10 max-sm:text-md">Digging into insights and user needs to uncover the story behind the solution.</p>
                 </div>
                 <div className="w-100 flex flex-col items-center">
-                    <PiStrategy className="text-white text-[250px] text-center" />
-                    <h1 className="text-white text-center text-3xl font-black">-2-</h1>
-                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150">STRATEGIZE</h1>
-                    <p className="text-white text-2xl text-center mt-10">Mapping out the vision and strategy to align creativity with goals. Clarity meets purpose here.</p>
+                    <PiStrategy className="text-white text-[250px] text-center max-sm:text-[130px]" />
+                    <h1 className="text-white text-center text-3xl font-black max-sm:text-2xl">-2-</h1>
+                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150 max-sm:text-4xl">STRATEGIZE</h1>
+                    <p className="text-white text-2xl text-center mt-10 max-sm:text-md">Mapping out the vision and strategy to align creativity with goals. Clarity meets purpose here.</p>
                 </div>
                 <div className="w-100 flex flex-col items-center">
-                    <SiAltiumdesigner className="text-white text-[250px] text-center" />
-                    <h1 className="text-white text-center text-3xl font-black">-3-</h1>
-                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150">DESIGN</h1>
-                    <p className="text-white text-2xl text-center mt-10">Where the magic happens! From concepts to visuals, its all about bringing bold ideas to life.</p>
+                    <SiAltiumdesigner className="text-white text-[250px] text-center max-sm:text-[130px]" />
+                    <h1 className="text-white text-center text-3xl font-black max-sm:text-2xl">-3-</h1>
+                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150 max-sm:text-4xl">DESIGN</h1>
+                    <p className="text-white text-2xl text-center mt-10 max-sm:text-md">Where the magic happens! From concepts to visuals, its all about bringing bold ideas to life.</p>
+                </div>
+                <div className="w-100 flex flex-col items-center ">
+                    <FaCode className="text-white text-[250px] text-center max-sm:text-[130px]" />
+                    <h1 className="text-white text-center text-3xl font-black max-sm:text-2xl">-4-</h1>
+                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150 max-sm:text-4xl">BUILD</h1>
+                    <p className="text-white text-2xl text-center mt-10 max-sm:text-md">Turning designs into reality with seamless development that bridges form and function.</p>
                 </div>
                 <div className="w-100 flex flex-col items-center">
-                    <FaCode className="text-white text-[250px] text-center" />
-                    <h1 className="text-white text-center text-3xl font-black">-4-</h1>
-                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150">BUILD</h1>
-                    <p className="text-white text-2xl text-center mt-10">Turning designs into reality with seamless development that bridges form and function.</p>
+                    <SiTestcafe className="text-white text-[250px] text-center max-sm:text-[130px]" />
+                    <h1 className="text-white text-center text-3xl font-black max-sm:text-2xl">-4-</h1>
+                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150 max-sm:text-4xl">TEST</h1>
+                    <p className="text-white text-2xl text-center mt-10 max-sm:text-md">Fine-tuning every detail through testing to ensure it works beautifully for users.</p>
                 </div>
                 <div className="w-100 flex flex-col items-center">
-                    <SiTestcafe className="text-white text-[250px] text-center" />
-                    <h1 className="text-white text-center text-3xl font-black">-4-</h1>
-                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150">TEST</h1>
-                    <p className="text-white text-2xl text-center mt-10">Fine-tuning every detail through testing to ensure it works beautifully for users.</p>
-                </div>
-                <div className="w-100 flex flex-col items-center">
-                    <FaRepeat className="text-white text-[250px] text-center" />
-                    <h1 className="text-white text-center text-3xl font-black">-4-</h1>
-                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150">REPEAT</h1>
-                    <p className="text-white text-2xl text-center mt-10">Refining, evolving, and growing—because great design is never one and done.</p>
+                    <FaRepeat className="text-white text-[250px] text-center max-sm:text-[130px]" />
+                    <h1 className="text-white text-center text-3xl font-black max-sm:text-2xl">-4-</h1>
+                    <h1 className="text-white text-6xl font-black text-center uppercase scale-y-150 max-sm:text-4xl">REPEAT</h1>
+                    <p className="text-white text-2xl text-center mt-10 max-sm:text-md">Refining, evolving, and growing—because great design is never one and done.</p>
                 </div>
             </section>
             <section className="w-full h-auto mt-80">
-<marquee scrollamount="5" loop="-1" class="text-9xl font-black text-white scale-y-150">
-  <span class="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> HELLO! 
-  <span class="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> こんにちは!  
-  <span class="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> HOLA!  
-  <span class="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> HALLO!  
-  <span class="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> OLÁ!  
-  <span class="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> مرحبا!  
-  <span class="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> CIAO!  
-  <span class="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> BONJOUR!  
-  <span class="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> HELLO!
-</marquee>            </section>
+                <marquee scrollamount="5" loop="-1" class="text-9xl font-black text-white scale-y-150 max-sm:text-5xl">
+                    <span className="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> HELLO!
+                    <span className="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> こんにちは!
+                    <span className="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> HOLA!
+                    <span className="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> HALLO!
+                    <span className="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> OLÁ!
+                    <span className="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> مرحبا!
+                    <span className="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> CIAO!
+                    <span className="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> BONJOUR!
+                    <span className="text-transparent [-webkit-text-stroke:1px_white]">SAY</span> HELLO!
+                </marquee>            </section>
         </main>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -5,56 +5,56 @@ export default function Home() {
     <main className="w-full h-auto  p-20 selection:bg-green-400 selection:text-black overflow-x-hidden">
       <section className="relative w-full h-auto ">
         <div className="w-full h-auto flex gap-15 max-lg:flex-col max-lg:justify-center">
-        {/* h1 heading */}
-        <div className="relative w-[50%] max-lg:w-full max-lg:justify-center max-lg:flex">
-        <h1 className="relative text-[210px] leading-[270px] font-black  text-white uppercase z-10 scale-y-150">full</h1>
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-e57ce5e6-2574-41c2-b0f3-7a376b239447.png?w=433&e=webp&nll=true" className="w-52 absolute top-[-20px] left-[30%] z-20" />
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-0db7a7a0-6eb6-4c66-afef-eaf0c2a3b838.png?w=433&e=webp&nll=true" className="w-52 absolute top-44 left-[12vw] z-0" />
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-558ef1cd-3f39-41a3-a26a-1bbe618a87e4.png?w=433&e=webp&nll=true" className="w-52 absolute top-[-90px] left-[26vw] z-0 " />
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-bb22ce71-9e22-41e6-ac38-71e9b528ad1a.png?w=433&e=webp&nll=true" className="w-52 absolute top-0 left-[32vw] z-0" />
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-b5b3a6ae-e2ab-4446-b1a2-d035a09f204b.png?w=433&e=webp&nll=true" className="w-52 absolute top-64 left-[29vw] z-0" />
-        
-        </div>
-        {/* h2 heading */} 
-        <div className="relative flex top-28 w-[50%]  max-lg:w-full max-lg:justify-center max-lg:left-0 max-lg:top-28 max-xl:left-24 max-xl:top-44">
-        <h1 className="relative left-[-25px] text-[200px] leading-[270px] font-black text-white uppercase z-10 scale-y-150 max-lg:text-[185px] max-xl:text-[10vw] max-2xl:text-[165px]">stack</h1>
-         <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-177dd55e-e715-4284-b969-21d3885a8fe9.png?w=433&e=webp&nll=true" className="w-52 absolute top-0 left-[20px] max-lg:left-[60px] z-10" />
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-560655ee-6349-4491-97ad-3eb333f37ae4.png?w=433&e=webp&nll=true" className="w-52 absolute top-[-120px] right-[140px] z-0" />
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-b4882ee6-9a76-4d9a-a2ad-37e5b3868afd.png?w=433&e=webp&nll=true" className="w-52 absolute top-[-50px] right-0 z-0" />
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-977f0a22-0cbe-4886-82ab-b638bd45eab2.png?w=433&e=webp&nll=true" className="w-52 absolute top-44 right-[60px] z-10" />
-        </div>
+          {/* h1 heading */}
+          <div className="relative w-[50%] max-lg:w-full max-lg:justify-center max-lg:flex">
+            <h1 className="relative text-[210px] leading-[270px] font-black  text-white uppercase z-10 scale-y-150 max-sm:text-[125px]">full</h1>
+            <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-e57ce5e6-2574-41c2-b0f3-7a376b239447.png?w=433&e=webp&nll=true" className="w-52 absolute top-[-20px] left-[30%] z-20 max-sm:w-36 max-sm:left-0 max-sm:top-0" />
+            <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-0db7a7a0-6eb6-4c66-afef-eaf0c2a3b838.png?w=433&e=webp&nll=true" className="w-52 absolute top-44 left-[12vw] z-0 max-sm:w-36" />
+            <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-558ef1cd-3f39-41a3-a26a-1bbe618a87e4.png?w=433&e=webp&nll=true" className="w-52 absolute top-[-90px] left-[26vw] z-0  max-sm:w-36" />
+            <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-bb22ce71-9e22-41e6-ac38-71e9b528ad1a.png?w=433&e=webp&nll=true" className="w-52 absolute top-0 left-[32vw] z-0 max-sm:w-36" />
+            <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-b5b3a6ae-e2ab-4446-b1a2-d035a09f204b.png?w=433&e=webp&nll=true" className="w-52 absolute top-64 left-[29vw] z-0 max-sm:w-36" />
+
+          </div>
+          {/* h2 heading */}
+          <div className="relative flex top-28 w-[50%]  max-lg:w-full max-lg:justify-center max-lg:left-0 max-lg:top-28 max-xl:left-24 max-xl:top-44">
+            <h1 className="relative left-[-25px] text-[200px] leading-[270px] font-black text-white uppercase z-10 scale-y-150 max-lg:text-[185px] max-xl:text-[10vw] max-2xl:text-[165px] max-sm:text-[90px] max-sm:text-center max-sm:ml-11">stack</h1>
+            <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-177dd55e-e715-4284-b969-21d3885a8fe9.png?w=433&e=webp&nll=true" className="w-52 absolute top-0 left-[20px] max-lg:left-[60px] z-10 max-sm:w-36" />
+            <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-560655ee-6349-4491-97ad-3eb333f37ae4.png?w=433&e=webp&nll=true" className="w-52 absolute top-[-120px] right-[140px] z-0 max-sm:w-36" />
+            <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-b4882ee6-9a76-4d9a-a2ad-37e5b3868afd.png?w=433&e=webp&nll=true" className="w-52 absolute top-[-50px] right-0 z-0 max-sm:w-36" />
+            <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-977f0a22-0cbe-4886-82ab-b638bd45eab2.png?w=433&e=webp&nll=true" className="w-52 absolute top-44 right-[60px] z-10 max-sm:w-36" />
+          </div>
         </div>
         {/* h3 heading */}
         <div className="relative w-full max-lg:top-10">
 
-        <h1 className="relative top-[-30px] text-[200px] text-center text-transparent leading-[270px] font-black uppercase pt-24 [-webkit-text-stroke:3px_white] scale-y-150 z-10 max-xl:text-[165px] max-lg:text-[210px] ">
-        <span className="hidden max-lg:inline">dev</span>
-        <span className="inline max-lg:hidden">engineer</span>
-        </h1>
+          <h1 className="relative top-[-30px] text-[200px] text-center text-transparent leading-[270px] font-black uppercase pt-24 [-webkit-text-stroke:3px_white] scale-y-150 z-10 max-xl:text-[165px] max-lg:text-[210px] ">
+            <span className="hidden max-lg:inline max-sm:text-[110px]">dev</span>
+            <span className="inline max-lg:hidden">engineer</span>
+          </h1>
 
-        {/* img images */}
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-7667d2e5-137b-4ec8-986d-38ca1218e281.png?w=433&e=webp&nll=true" className="w-52 absolute top-28 left-10 z-0" />
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-7ca2aee2-7b4b-4c1c-aeff-7892cd75c80e.png?w=433&e=webp&nll=true" className="w-52 absolute top-36 left-52 z-0 opacity-75" />
-        <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-1e93a821-903c-462a-a300-f845f1f3a235.png?w=433&e=webp&nll=true" className="w-52 absolute bottom-0 right-100 z-10 max-lg:right-10" />
+          {/* img images */}
+          <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-7667d2e5-137b-4ec8-986d-38ca1218e281.png?w=433&e=webp&nll=true" className="w-52 absolute top-28 left-10 z-0 max-sm:w-36" />
+          <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-7ca2aee2-7b4b-4c1c-aeff-7892cd75c80e.png?w=433&e=webp&nll=true" className="w-52 absolute top-36 left-52 z-0 opacity-75 max-sm:w-36" />
+          <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-1e93a821-903c-462a-a300-f845f1f3a235.png?w=433&e=webp&nll=true" className="w-52 absolute bottom-0 right-100 z-10 max-lg:right-10 max-sm:w-36" />
         </div>
-       
+
       </section>
       {/* short Abouts us */}
-      <section className="w-full h-auto mt-20 p-56 max-lg:p-10 max-lg:mt-100">
+      <section className="w-full h-auto mt-20 p-56 max-lg:p-10 max-lg:mt-100 max-sm:p-0">
         <p className="text-4xl font-black text-[#B2B2B2] text-center">Hey there ✰! I’m Muhammad Farooq — a MERN Stack & Next.js Developer ⚡ based in Pakistan.
           I specialize in building modern, high-performing web applications that are fast, scalable, and user-friendly. From crafting clean front-end experiences with React & Tailwind CSS to developing robust back-end solutions with Node.js and MongoDB, I love turning ideas into impactful digital products that really work.</p>
       </section>
       <section className="w-full h-auto pb-20 max-lg:mt-100">
-        <h1 className="text-left font-[cursive] text-[#DADADA] text-[250px] leading-[170px] rotate-[-2deg] max-lg:text-[220px]">Work</h1>
-        <h2 className="text-right leading-[170px] uppercase text-transparent text-[170px] font-black [-webkit-text-stroke:2px_#DADADA] scale-y-150 tracking-tighter max-lg:text-[104px]">buildnthat</h2>
-        <h2 className="text-right uppercase text-transparent text-[170px] font-black [-webkit-text-stroke:2px_#DADADA] scale-y-150 tracking-tighter max-lg:text-[110px]">speaks</h2>
-        <p className="text-left uppercase text-[#DADADA] font-black w-[400px] text-2xl">Peek into a lineup of projects where logic meets innovation.</p>
+        <h1 className="text-left font-[cursive] text-[#DADADA] text-[250px] leading-[170px] rotate-[-2deg] max-lg:text-[220px] max-sm:text-[110px]">Work</h1>
+        <h2 className="text-right leading-[170px] uppercase text-transparent text-[170px] font-black [-webkit-text-stroke:2px_#DADADA] scale-y-150 tracking-tighter max-lg:text-[104px] max-sm:text-[45px] max-sm:text-start max-sm:leading-[70px]">buildnthat</h2>
+        <h2 className="text-right uppercase text-transparent text-[170px] font-black [-webkit-text-stroke:2px_#DADADA] scale-y-150 tracking-tighter max-lg:text-[110px] max-sm:text-[45px] max-sm:leading-[70px]">speaks</h2>
+        <p className="text-left uppercase text-[#DADADA] font-black w-[400px] text-2xl max-sm:text-sm">Peek into a lineup of projects where logic meets innovation.</p>
       </section>
       <section className="w-full h-auto flex flex-wrap justify-start items-center gap-5 mt-20">
         <div className="relative w-[49%] h-[1000px] flex flex-col justify-between border-2 border-[#D3D3D3] mt-10 max-lg:w-full">
           <div className="w-full h-auto flex flex-col items-center justify-center text-center p-20">
-            <h1 className="text-[#D3D3D3] text-2xl scale-y-170 font-black tracking-tighter">THE EXIT INTERVIEW</h1>
-            <h1 className="relative top-5 text-[#D3D3D3] text-5xl scale-y-150 font-black uppercase tracking-tighter">The Developer’s Experiment</h1>
+            <h1 className="text-[#D3D3D3] text-2xl scale-y-170 font-black tracking-tighter max-sm:text-xl">THE EXIT INTERVIEW</h1>
+            <h1 className="relative top-5 text-[#D3D3D3] text-5xl scale-y-150 font-black uppercase tracking-tighter max-sm:text-3xl max-sm:mt-4">The Developer’s Experiment</h1>
           </div>
           <div className=" w-full h-auto flex items-center justify-center">
             <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-136c2817-193b-4170-8397-2b27c242b5d5.png?e=webp&nll=true&cX=0&cY=0.5&cW=1368&cH=1182" className="w-full rotate-10" />
@@ -62,8 +62,8 @@ export default function Home() {
         </div>
         <div className="relative w-[49%] h-[1000px] flex flex-col justify-between border-2 border-[#D3D3D3] mt-10 max-lg:w-full">
           <div className="w-full h-auto flex flex-col items-center justify-center text-center p-20">
-            <h1 className="text-[#D3D3D3] text-2xl scale-y-170 font-black tracking-tighter">THE STUDIO</h1>
-            <h1 className="relative top-5 text-[#D3D3D3] text-5xl scale-y-150 font-black uppercase tracking-tighter">a home for Musician</h1>
+            <h1 className="text-[#D3D3D3] text-2xl scale-y-170 font-black tracking-tighter max-sm:text-xl">THE STUDIO</h1>
+            <h1 className="relative top-5 text-[#D3D3D3] text-5xl scale-y-150 font-black uppercase tracking-tighter max-sm:text-3xl max-sm:mt-4">a home for Musician</h1>
           </div>
           <div className=" w-full h-auto flex items-center justify-center">
             <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-60810f65-1f66-440a-a5b9-4e7db2771219.png?w=1459&e=webp&nll=true" className="w-full rotate-20" />
@@ -71,8 +71,8 @@ export default function Home() {
         </div>
         <div className="relative w-[49%] h-[1000px] flex flex-col justify-between border-2 border-[#D3D3D3] mt-10 max-lg:w-full">
           <div className="w-full h-auto flex flex-col items-center justify-center text-center p-20">
-            <h1 className="text-[#D3D3D3] text-2xl scale-y-170 font-black tracking-tighter">THE STUDIO</h1>
-            <h1 className="relative top-5 text-[#D3D3D3] text-5xl scale-y-150 font-black uppercase tracking-tighter">a home for Musician</h1>
+            <h1 className="text-[#D3D3D3] text-2xl scale-y-170 font-black tracking-tighter max-sm:text-xl">THE STUDIO</h1>
+            <h1 className="relative top-5 text-[#D3D3D3] text-5xl scale-y-150 font-black uppercase tracking-tighter max-sm:text-3xl max-sm:mt-4">a home for Musician</h1>
           </div>
           <div className=" w-full h-auto flex items-center justify-center">
             <img src="https://i-p.rmcdn.net/6169a98c239f1d001467eba5/5146291/image-60810f65-1f66-440a-a5b9-4e7db2771219.png?w=1459&e=webp&nll=true" className="w-full rotate-20" />
@@ -82,8 +82,8 @@ export default function Home() {
       <section className="w-full h-auto mt-10">
         <Design />
       </section>
-      <section className="w-full  mt-10">
-        <Contact/>
+      <section className="w-full h-auto mt-10">
+        <Contact />
       </section>
     </main>
   );
