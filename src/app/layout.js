91fae7b0e044d69a106710f1c7787f50a56e14be
Farooqfar <info4895@gmail.com>
2025-09-03 23:41:40 +0500

    faci

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,9 @@ export const metadata = {
   title: "Muhammad Farooq",
   description:
     "I’m Muhammad Farooq, a passionate MERN Stack and Frontend Developer with expertise in building modern, responsive, and high-performance web applications. I specialize in React, Next.js, Tailwind CSS, and JavaScript, crafting clean and scalable solutions that deliver great user experiences. On the backend, I work with Node.js, Express, and MongoDB to create powerful full-stack applications. With a strong foundation in web development and problem-solving, I enjoy turning ideas into functional, user-friendly digital products. Explore my portfolio to see projects that showcase my skills in frontend design, full-stack development, and performance optimization.",
+  icons: {
+    icon: "/faivcon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
