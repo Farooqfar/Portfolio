725346a9adfedbb129ff8f332cfdaf78ae62d7e7
Farooqfar <info4895@gmail.com>
2025-09-07 00:48:00 +0500

    aboutus

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <main>
+      <section>
+        <h1>about</h1>
+        <p>
+          Hey there ✰! I’m Muhammad Farooq, a passionate Web Developer 💻 based
+          in Pakistan. I spend my time turning ideas into interactive,
+          responsive, and impactful digital experiences—making websites not only
+          look GREAT but also work even better ☜.
+        </p>
+        <p>
+          For the past year ☟ I’ve been living the ✰ developer dream—taking
+          concepts and transforming them into clean, functional, and
+          performance-driven code ❀. From front-end design to back-end logic, I
+          love building projects that are both creative and technically solid.
+        </p>
+        <p>
+          My focus ✎ is always on creating web experiences that truly connect
+          with people. Whether it’s crafting smooth UIs, optimizing performance,
+          or solving tough coding challenges, I aim to deliver solutions that
+          are meaningful, user-friendly, and unforgettable.
+        </p>
+      </section>
+    </main>
+  );
 }
