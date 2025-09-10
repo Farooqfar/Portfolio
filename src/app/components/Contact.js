4d97152f16d89fd9aa5fe0c2ba3dbab7cacd4502
Farooqfar <info4895@gmail.com>
2025-09-11 00:01:13 +0500

    slv

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function Contact() {
   return (
     <section className="mt-32 flex items-center justify-center p-5 ">
-      <Link href="mailto:info4895@gmail.com" className="w-[740px] rounded-4xl text-white text-center text-4xl font-bold border-2 border-white p-2 hover:text-black hover:bg-white max-md:w-fit max-md:text-xl">
+      <Link
+        href="mailto:info4895@gmail.com"
+        className="w-full rounded-4xl text-white text-center text-4xl font-bold border-2 border-white p-2 hover:text-black hover:bg-white max-md:w-fit max-md:text-xl"
+      >
         info4895@gmail.com
       </Link>
     </section>
