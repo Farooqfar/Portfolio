95150379de0a760c51daa8036d7468cde9af4382
Farooqfar <info4895@gmail.com>
2025-09-11 00:03:48 +0500

    slov com

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
     <section className="mt-32 flex items-center justify-center p-5 ">
       <Link
         href="mailto:info4895@gmail.com"
-        className="w-full rounded-4xl text-white text-center text-4xl font-bold border-2 border-white p-2 hover:text-black hover:bg-white max-md:w-fit max-md:text-xl"
+        className="w-full rounded-4xl text-white text-center text-4xl font-bold border-2 border-white p-2 hover:text-black hover:bg-white max-md:w-full max-md:text-xl"
       >
         info4895@gmail.com
       </Link>
